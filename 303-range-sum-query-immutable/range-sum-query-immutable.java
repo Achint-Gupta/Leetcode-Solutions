376fe03085061649c2e03f91fb74119bc034fc6c
Achint Gupta <achintg2007@gmail.com>
2026-09-17 21:48:45 +0530

    Time: 9 ms (25.80%) | Memory: 47.6 MB (62.20%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -2,14 +2,14 @@ class NumArray {
     int[] nums;
     public NumArray(int[] nums) {
         this.nums=nums;
+        for (int i = 1; i < this.nums.length; ++i)
+            this.nums[i] += this.nums[i-1]; 
     }
     
     public int sumRange(int left, int right) {
-       int sum=0;
-       for(int i=left;i<=right;i++){
-        sum+=nums[i];
-       } 
-       return sum;
+       if(left==0) return nums[right];
+       else return nums[right] - nums[left-1];
+       
     }
 }
 
